78f3c744d9d907a1e18833d0a4b5f62d50dec03b
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-03-18 15:24:53 -0400

    feat!: Remove CommonJS files"

--- tests/pkg.test.cjs
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview Tests that Common JS can access npm package.
- */
-
-const assert = require("node:assert");
-const { Client } = require("../");
-assert.strictEqual(typeof Client, "function");
-console.log("CommonJS load: success");
